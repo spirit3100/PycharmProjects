ab05ed10b59ed9cbbe4dc3adad259ab1e8fdcadd
spirit3100 <spirit3100@gmail.com>
2022-05-15 22:54:03 +0300

    change pawns

--- chess/func_figures.py
+++ chess/func_figures.py
@@ -4,8 +4,7 @@ def blank_desk():
 
     s = {}
     for i in vertical:
-        for v in horizontal:
-            s[i + str(v)] = (vertical.index(i) , horizontal.index(v)), None, '  ', None
+        for v in horizontal: s[i + str(v)] = (vertical.index(i) , horizontal.index(v)), None, '  ', None
 
     return s
 
@@ -32,36 +31,72 @@ def rook(desk, cage_x_y):
 
     x = cage_x_y[1]
     a = []
-    for i in range(-8, 8):
-        if x[0] + i >= 0 and x[0] + i < 8:
-            c = x[0] + i
-            v = x[1]
-            a.append((c, v))
-        if x[1] + i >= 0 and x[1] + i < 8:
-            c = x[0]
-            v = x[1] + i
-            a.append((c,v))
+    b = []
+    cc = []
+    d = []
+    # for i in range(0, 8):
+    #
+    #     c = x[0] + i
+    #     v = x[1]
+    #     if c > 0 and c < 8:
+    #         a.append((c, v))
+    #     c = x[0]
+    #     v = x[1] + i
+    #     if v > 1 and v < 8:
+    #         b.append((c, v))
 
-    return find_permission(a)
+    # for i, k in desk.items():
+    #     if k[1] != None:
+    #         print(f'i: {k[1]}')
+    #         b = i
+    #         break
+    #     else: b.append(i)
+    # print(f'b {b}')
+    # # for i in range(-8, 0):
+    # #     c = x[0] + i
+    # #     v = x[1]
+    # #     if c > 0 and c < 8:
+    # #         cc.append((c,v))
+    # #     c = x[0]
+    # #     v = x[1] + i
+    # #     if v > 0 and v < 8:
+    # #         d.append((c,v))
+    # print(a)
+    # print(b)
+    # print(cc)
+    # print(d)
+    # bb = []
+    # for i in desk:
+    #     v = desk.get(i)
+    #     if desk.get(i):
+    #         # print(desk.get(i))
+    #         bb.append(i)
+    # print(f'get fig: {[desk.get(i)[2] for i in a]}')
+    # print(f'b: {bb}')
+    # print(f'\ncage: {cage_x_y[0]}')
+    # print(f'x_y: {cage_x_y[1]}')
+    # print(f'steps: {a}\n')
+
+    # return find_permission(a)
 
 def bishop(desk, cage_x_y):
+
+    def find(list_s, x_y, int_1, int_2):
+
+        x, y = x_y
+        for i in range(8):
+            x += int_1
+            y += int_2
+            list_s.append((x, y))
+
+        return list_s
 
     x_y = cage_x_y[1]
     steps = []
     x = ((1,1), (-1,-1), (-1,1), (1,-1))
-    for i in x: steps = bish_find(steps, x_y, i[0], i[1])
+    for i in x: steps = find(steps, x_y, i[0], i[1])
 
     return find_permission(steps)
-
-def bish_find(list_s, x_y, int_1, int_2):
-
-    x, y = x_y
-    for i in range(8):
-        x += int_1
-        y += int_2
-        list_s.append((x, y))
-
-    return list_s
 
 def queen(desk, cage_x_y):
 
@@ -70,9 +105,9 @@ def queen(desk, cage_x_y):
 
     return a + b
 
-def king(desk, x_y):
+def king(desk, cage_x_y):
 
-    x = x_y[1]
+    x = cage_x_y[1]
     a, b, c, d = x[0] + 1, x[0] -1, x[1] + 1, x[1] - 1
     steps = [(a, c), (b, c), (a, d), (b, d), (x[0], c), (x[0], d), (a, x[1]), (b, x[1])]
 
@@ -83,9 +118,12 @@ def pawns(desk, cage_x_y):
     cage, (x, y) = cage_x_y
     steps = []
     color = desk.get(cage)[3]
+    x_y = desk.get(cage)[0]
     if color == 'white':
-        steps = [(x, y + 1), (x, y + 2), (x - 1, y + 1), (x + 1, y + 1)]
+        steps = [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)]
+        if x_y[1] == 1: steps.append((x, y + 2))
     elif color == 'black':
-        steps = [(x, y - 1), (x, y - 2), (x - 1, y - 1), (x + 1, y - 1)]
+        steps = [(x, y - 1), (x - 1, y - 1), (x + 1, y - 1)]
+        if x_y[1] == 6: steps.append((x, y - 2))
 
     return find_permission(steps)
--- chess/main.py
+++ chess/main.py
@@ -1,15 +1,20 @@
 from logic_chess import *
+# игра в шахматы
 
+# при первом выборе ячейки, идет проверка  на все возможные варианты хода фигуры этой ячейки и выводится на экран.
+# при втором выборе ячейки, происводится шаг фигурой.
+#
+# дальнейшие действия: запрет на битие своей фигуры
 
 def main():
 
     print()
     desk = show_desk(create_desk())
     while True:
-        cage_1 = step('your figure position: ')
+        cage_1 = step('your figure position or exit: ')
         f_desk, steps = find_figure(desk, cage_1)
         print(f'permission steps: {steps}')
-        cage_2 = step('your step: ')
+        cage_2 = step('your step or exit: ')
         update_desk(desk, player_step, (cage_1, cage_2))
         if cage_1 == 'exit' or cage_2 == 'exit': break
 
