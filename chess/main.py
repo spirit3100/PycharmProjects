7393d0e5a524af258ec8898a1bd44f104f423f1d
spirit3100 <spirit3100@gmail.com>
2022-05-15 17:34:46 +0300

    change sintax main

--- chess/main.py
+++ chess/main.py
@@ -3,16 +3,15 @@ from logic_chess import *
 
 def main():
 
-    s = create_desk()
     print()
-    f = show_desk(s)
+    desk = show_desk(create_desk())
     while True:
-        a = step('your figure position: ')
-        f_d, st = find_figure(f, a)
-        print(f'permission steps: {st}')
-        b = step('your step: ')
-        update_desk(f, player_step, (a, b))
-        if a == 'exit' or b == 'exit': break
+        cage_1 = step('your figure position: ')
+        f_desk, steps = find_figure(desk, cage_1)
+        print(f'permission steps: {steps}')
+        cage_2 = step('your step: ')
+        update_desk(desk, player_step, (cage_1, cage_2))
+        if cage_1 == 'exit' or cage_2 == 'exit': break
 
 
 main()
